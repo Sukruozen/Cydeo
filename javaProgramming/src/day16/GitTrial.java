349ba2ad39ff706ebd2fdd4e231bb0b9175b8753
Sukru  Ozen <sukruozen@gmail.com>
2022-06-09 23:30:31 +0300

    trial

---
+++ javaProgramming/src/day16/GitTrial.java
@@ -0,0 +1,10 @@
+package day16;
+
+import java.util.Scanner;
+
+public class GitTrial {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Git trial 1...");
+    }
+}
